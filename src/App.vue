5f02a99b84270385470bf3f95e86fdabad754e97
Oli Newsham <scarylooking@users.noreply.github.com>
2021-10-16 17:26:04 +0100

    Added instructions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <FigmentForm />
+  <div class="content">
+    <h1>Step 1: Check if Holodeck-2 has already been added</h1>
+    <p>
+      If you already have Holodeck-2 or Testnet configured in Keplr you will
+      need to remove it first, or this tool will do nothing.
+    </p>
+
+    <ol class="guide">
+      <li>
+        Open and unlock Keplr, then click the name of the currently selected
+        chain at the top
+      </li>
+      <img alt="Vue logo" src="./assets/chainname.png" />
+      <li>
+        Scroll to the bottom, and check Holodeck-2 or Testnet are not listed
+        under Beta Support
+      </li>
+      <img alt="Vue logo" src="./assets/betasupport.png" />
+      <li>If they are, click the X next to the chain name to remove it</li>
+    </ol>
+
+    <FigmentForm />
+  </div>
 </template>
 
 <script>
@@ -20,8 +41,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#app .content {
+  width: 800px;
+  margin: 0 auto;
+}
+
+ol.guide li {
+  padding: 1em 0;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
 }
 </style>
--- src/components/FigmentForm.vue
+++ src/components/FigmentForm.vue
@@ -1,14 +1,68 @@
 <template>
   <div>
-    <h1>Add Figment Holodeck-2 to Keplr</h1>
-    <p>
-      Sign up for a free
-      <a href="https://datahub.figment.io/sign_up" target="_blank">DataHub</a>
-      account, then paste your API key below and hit the Add button
-    </p>
-    <input v-model="apiKey" placeholder="DataHub API Key" size="32" />
+    <h1>Step 2: Get an API Key from Figment</h1>
+
+    <ol class="guide">
+      <li>
+        If you don't have one sign up for a free
+        <a href="https://datahub.figment.io/sign_up" target="_blank">DataHub</a>
+        account.
+      </li>
+      <li>
+        Login to DataHub and go to the
+        <a href="https://datahub.figment.io/services/secret" target="_blank"
+          >Secret Network</a
+        >
+        page
+      </li>
+      <li>Find your API key. it should be 32 characters long and click Copy</li>
+      <img alt="Vue logo" src="../assets/apikeys.png" />
+    </ol>
+
+    <h1>Step 3: Add Holodeck-2 to Keplr</h1>
+
+    <ol class="guide">
+      <li>Paste your API key into this box...</li>
+
+      <input v-model="apiKey" placeholder="DataHub API Key" size="32" />
+
+      <li>
+        Click the add button below. It will only enable when you have entered an
+        API key
+      </li>
+
+      <button @click="addNetwork()" :disabled="isDisabled">
+        Add Holodeck-2
+      </button>
+
+      <li>Keplr should open an approval window. Read it, then click Approve</li>
+
+      <img alt="Vue logo" src="../assets/keplrapprove.png" />
+
+      <li>
+        If no error appears then Holodeck-2 was successfully added to Keplr.
+      </li>
+    </ol>
+
+    <h1>Step 4: Switch from Mainnet to Holodeck-2 in Keplr</h1>
+
+    <ol class="guide">
+      <li>
+        Open and unlock Keplr, then click the name of the currently selected
+        chain at the top
+      </li>
+      <img alt="Vue logo" src="../assets/chainname.png" />
+      <li>
+        Scroll to the bottom then click Figment Holodeck-2 under Beta Support
+      </li>
+      <img alt="Vue logo" src="../assets/figmentholodeck.png" />
+      <li>
+        Congrats! Keplr is now using the Secret Network Holodeck-2 testnet chain
+        via Figment
+      </li>
+    </ol>
+
     <br />
-    <button @click="addNetwork()" :disabled="isDisabled">Add Holodeck-2</button>
   </div>
 </template>
 
@@ -78,18 +132,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
